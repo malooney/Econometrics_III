a18490084e96a128c91126c09aeb20e830bd42ec
malooney <matthew.looney@icloud.com>
2017-09-12 20:57:08 -0500

    re-write matlab loop, faster

--- HW1_Matlab.m
+++ HW1_Matlab.m
@@ -1,16 +1,26 @@
 
-n=1000000;
-i=1;
 
-for i= 1:n
+tic
+n=100000;
+rep=1000;
+i=1;
+temp= zeros([rep 3]);
+
+for i= 1:rep
+    
 e= 0+ 5*rand(n, 1);
 x1= 100+ 7* rand(n, 1);
 x2= 1000+ 10* rand(n, 1);
 
-y= 5+ 4*x1+ 0.7*x2 +e;
+y= 5+ 4* x1+ 0.7* x2 +e;
 
 X=[ones(n, 1) x1 x2];
-b_ols= inv(X'*X)*X'*y;
+
+b_ols= (X'*X) \ (X'*y);
 
 temp(i,1:3)= b_ols;
+
 end
+
+mean(temp)
+toc
